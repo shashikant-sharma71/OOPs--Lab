1b1d82745516ea9b195b1232dd58d1dd8f3d63ba
shashikant-sharma71 <159624249+shashikant-sharma71@users.noreply.github.com>
2024-09-08 13:48:31 +0530

    Add files via upload

---
+++ swapnumbers.cpp
@@ -0,0 +1,18 @@
+
+// Swap two numbers 
+
+
+#include<iostream>
+using namespace std;
+int main(){
+int a=10;
+int b=20;
+ cout<<"Befor Swaping the numbers"<<endl;
+ cout<<"a="<<a <<endl <<"b="<<b<<endl;
+int swap=a;
+a=b;
+b=swap;
+cout<<"After Swaping"<<endl;
+cout<<"a="<<a<<endl<<"b="<<b;
+}
+
